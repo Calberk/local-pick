d29b2c9899a215556a8f9af9ea558e19c80bf165
Calberk <park.edmund@gmail.com>
2019-02-19 15:55:13 -0800

    created initial react native app

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Lets test this out!</Text>
+        <Text>This is the 2nd line of React Native code</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
