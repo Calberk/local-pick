3e613012403ec60ec3b86637097cb9f57f216bb7
Edmund Park <41177897+Calberk@users.noreply.github.com>
2019-03-04 15:01:44 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
   // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyDgMVXwPXFTlq8x6cZaPDsqoHeim-6HNI0",
-    authDomain: "parapxl-local-picks.firebaseapp.com",
-    databaseURL: "https://parapxl-local-picks.firebaseio.com",
-    projectId: "parapxl-local-picks",
-    storageBucket: "parapxl-local-picks.appspot.com",
-    messagingSenderId: "921022708448"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "m",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
 
   firebase.initializeApp(config);
